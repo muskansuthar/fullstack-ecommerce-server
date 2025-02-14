6071d85c022732e83301c0e475e9b81879380d54
muskan <muskansuthar89@gmail.com>
2025-02-14 17:25:40 +0530

    some chages

--- app.js
+++ app.js
@@ -8,15 +8,7 @@ import authJwt from './helper/jwt.js';
 //Routes
 import categoryRoutes from './routes/categories.js';
 import productRoutes from './routes/product.js';
-import productWeightRoutes from './routes/productWeight.js';
-import productRamsRoutes from './routes/productRams.js';
-import productSizeRoutes from './routes/productSize.js';
 import userRoutes from './routes/user.js';
-import cartRoutes from './routes/cart.js';
-import myListRoutes from './routes/myList.js';
-import productReviewsRoutes from './routes/productReviews.js';
-import ordersRoutes from './routes/orders.js';
-import homeBannerRoutes from './routes/homeBanner.js';
 import searchRoutes from './routes/search.js';
 
 const app = express()     
@@ -32,15 +24,7 @@ app.use(authJwt())
 app.use('/uploads', express.static("uploads"))
 app.use('/api/user', userRoutes)
 app.use('/api/category', categoryRoutes)
-app.use('/api/products', productRoutes)
-app.use('/api/productWeight', productWeightRoutes)
-app.use('/api/productRAMS', productRamsRoutes)  
-app.use('/api/productSize', productSizeRoutes)
-app.use('/api/cart', cartRoutes)
-app.use('/api/my-list', myListRoutes)
-app.use('/api/productReviews', productReviewsRoutes)
-app.use('/api/orders', ordersRoutes)
-app.use('/api/homeBanner', homeBannerRoutes)   
+app.use('/api/products', productRoutes)  
 app.use('/api/search', searchRoutes)
 
 //Database
